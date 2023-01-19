cb5d7dc3a328fdc23da87ce68fc9b9f23fa3e63f
Jordan Dukes <jordandukes@Jordans-MBP.askey.com>
2023-01-19 15:42:41 -0800

    Added packages and prompts for user input

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generaeMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+     type: 'input',
+     message: 'What is your user name?',
+     name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+       },
+       {
+        type: 'list',
+        message: 'What is your user name?',
+        name: 'username',
+        choices: ["A", "B", "C"],
+        default: "B"
+
+    },
+       
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
